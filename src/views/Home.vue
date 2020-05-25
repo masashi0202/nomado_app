b1769525efd28dd2837cf823d83d7501934d0a66
Masashi Yoshino <1993.masashi@gmail.com>
2020-05-25 12:45:37 +0900

    edit_HelloWorld-comp

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to nomado-map2"/>
+    <HelloWorld msg="Welcome to nomado-map1"/>
   </div>
 </template>
 
